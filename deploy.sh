b55471f05b4b76b8fa45f206c0afd6455eee62d4
Filip Matzner <floop@floop.cz>
2017-08-07 18:04:58 +0200

    Cleanup deployment script

--- deploy.sh
+++ deploy.sh
@@ -31,21 +31,13 @@ git submodule init
 git submodule update
 
 # copy neovim colorscheme and config
-mkdir -vp ~/.config/nvim
-mkdir -vp ~/.config/nvim/autoload
-mkdir -vp ~/.config/nvim/bitmaps
 mkdir -vp ~/.config/nvim/colors
-mkdir -vp ~/.config/nvim/doc
-ln -srfv base16-vim/colors/*.vim ~/.config/nvim/colors || true
+ln -srfv base16-vim/colors/*.vim ~/.config/nvim/colors/
 ln -srfv _vimrc ~/.config/nvim/init.vim
 
 # copy vim colorscheme and config
-mkdir -vp ~/.vim
-mkdir -vp ~/.vim/autoload
-mkdir -vp ~/.vim/bitmaps
 mkdir -vp ~/.vim/colors
-mkdir -vp ~/.vim/doc
-ln -srfv base16-vim/colors/*.vim ~/.vim/colors || true
+ln -srfv base16-vim/colors/*.vim ~/.vim/colors/
 ln -srfv _vimrc ~/.vimrc
 
 # copy ideavim config
@@ -66,12 +58,4 @@ ln -srfv _clang-format ~/.clang-format
 
 # copy binaries
 mkdir -vp ~/bin
-ln -srfv bin/* ~/bin || true
-
-# set up dropbox (https://wiki.archlinux.org/index.php/dropbox)
-# 1. disable auto-update (so that systemd daemon works)
-#rm -rf ~/.dropbox-dist
-#install -dm0 ~/.dropbox-dist
-# 2. allow more file watchers
-#sudo echo "fs.inotify.max_user_watches = 100000" >> /etc/sysctl.d/99-sysctl.conf
-#sudo sysctl --system
+ln -srfv bin/* ~/bin/
