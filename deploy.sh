5ecfea4ed895f8250c5d01e4e36c05c65b993411
Filip Matzner <floop@floop.cz>
2018-01-30 19:55:49 +0100

    rm moves to trash

--- deploy.sh
+++ deploy.sh
@@ -29,7 +29,7 @@ if [ "$1" = "--install" ]; then
   fi
 
   # install basic packages
-  sudo $pacman_bin git neovim zsh tmux htop zip unzip mc xsel curl
+  sudo $pacman_bin git neovim zsh tmux htop zip unzip mc xsel curl trash-cli
 fi
 
 # get submodules
